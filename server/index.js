308bb7dca7ecf2f9934bb23cfe196fa3684655f2
Ctag14 <95243929+Ctag14@users.noreply.github.com>
2022-11-01 14:45:40 -0400

    "adaptable config 2"

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const io = new Server(server, {
     methods: "*",
   },
 });
-const PORT = 3001;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
