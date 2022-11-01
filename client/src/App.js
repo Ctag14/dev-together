4949951814988f2306d1cbddbeafec2c5d0ee737
Ctag14 <95243929+Ctag14@users.noreply.github.com>
2022-11-01 14:44:47 -0400

    adaptable config

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import { v4 as uuidv4 } from "uuid";
 import Editor from "./components/Editor";
 import Participants from "./components/Participants";
 
-const socket = io("https://dev-together.vercel.app/server/index.js");
+const socket = io("https://dev-together.vercel.app/");
 
 function App() {
   const [displayName, setDisplayName] = useState("");
